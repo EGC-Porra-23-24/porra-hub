b35de326fc1ffb4a36adfa01c301257ec59a4f3b
Jorge Gómez de Tovar <jorgomde@alum.us.es>
2024-11-16 13:51:09 +0000

    feat(dataset): backend inicial

--- app/modules/dataset/routes.py
+++ app/modules/dataset/routes.py
@@ -225,7 +225,7 @@ def create_from_zip():
     return render_template("dataset/upload_zip.html", form=form)
 
 
-@dataset_bp.route("/dataset/upload/file/zip")
+@dataset_bp.route("/dataset/file/upload/zip", methods=["POST", "GET"])
 @login_required
 def upload_from_zip():
     pass
