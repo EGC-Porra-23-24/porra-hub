9ddc8f00f5864335f15428ccdc2e81f1f35a290b
Albertoescobarsanchez <albescsan1@alum.us.es>
2024-12-04 22:50:22 +0000

    feat(auth): Verificación de email

--- app/modules/auth/routes.py
+++ app/modules/auth/routes.py
@@ -12,6 +12,7 @@ user_profile_service = UserProfileService()
 
 
 @auth_bp.route("/signup/", methods=["GET", "POST"])
+@auth_bp.route("/signup/", methods=["GET", "POST"])
 def show_signup_form():
     if current_user.is_authenticated:
         return redirect(url_for('public.index'))
@@ -19,21 +20,37 @@ def show_signup_form():
     form = SignupForm()
     if form.validate_on_submit():
         email = form.email.data
+        name = form.name.data
+        surname= form.surname.data
+        password = form.password.data
         if not authentication_service.is_email_available(email):
             return render_template("auth/signup_form.html", form=form, error=f'Email {email} in use')
 
         try:
-            user = authentication_service.create_with_profile(**form.data)
+            user_data = {
+                'email': email,
+                'name': name,
+                'surname':surname,
+                'password': password
+            }
+            authentication_service.send_verification_email(user_data) 
         except Exception as exc:
-            return render_template("auth/signup_form.html", form=form, error=f'Error creating user: {exc}')
+            return render_template("auth/signup_form.html", form=form, error=f'Error sending verification email: {exc}')
 
-        # Log user
-        login_user(user, remember=True)
-        return redirect(url_for('public.index'))
+        return render_template("auth/signup_success.html", message="Check your inbox to verify your account.")
 
     return render_template("auth/signup_form.html", form=form)
 
-
+@auth_bp.route('/verify/<token>')
+def verify_email(token):
+    # Verifica el token y obtiene los datos del usuario asociados
+    user = authentication_service.verify_token(token)
+    
+    if user is None:
+        return render_template("auth/verification_failed.html", message="El enlace de verificación es inválido o ha expirado.")
+    else:
+        return render_template("auth/verification_success.html", message="Email verified and user account created.")
+    
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
--- app/modules/auth/services.py
+++ app/modules/auth/services.py
@@ -9,6 +9,12 @@ from app.modules.profile.repositories import UserProfileRepository
 from core.configuration.configuration import uploads_folder_name
 from core.services.BaseService import BaseService
 
+from itsdangerous import URLSafeTimedSerializer
+from flask import current_app
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from flask import url_for
 
 class AuthenticationService(BaseService):
     def __init__(self):
@@ -31,7 +37,8 @@ class AuthenticationService(BaseService):
             password = kwargs.pop("password", None)
             name = kwargs.pop("name", None)
             surname = kwargs.pop("surname", None)
-
+            if not self.is_email_available(email):
+                return None
             if not email:
                 raise ValueError("Email is required.")
             if not password:
@@ -60,6 +67,7 @@ class AuthenticationService(BaseService):
             raise exc
         return user
 
+
     def update_profile(self, user_profile_id, form):
         if form.validate():
             updated_instance = self.update(user_profile_id, **form.data)
@@ -79,3 +87,47 @@ class AuthenticationService(BaseService):
 
     def temp_folder_by_user(self, user: User) -> str:
         return os.path.join(uploads_folder_name(), "temp", str(user.id))
+    
+    def generate_verification_token(self, user_data):
+        serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        return serializer.dumps(user_data, salt="user-registration-salt")
+
+    def verify_token(self, token):
+        serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        try:
+            user_data = serializer.loads(token, salt="user-registration-salt", max_age=3600)
+        except Exception as e:
+            print(f"Error verifying token: {e}")
+            return None
+        
+        user=self.create_with_profile(**user_data)
+        return user
+    def send_verification_email(self, user_data):
+        # geneacion token
+        user_email=user_data.get("email")
+        if not self.is_email_available(user_email):
+            return
+        token = self.generate_verification_token(user_data)
+        verification_link = url_for('auth.verify_email', token=token, _external=True)
+        # configuración correo que envia la verificacion
+        sender_email = os.getenv('VERIFICATION_EMAIL')
+        sender_password = os.getenv('VERIFICATION_EMAIL_PASSWORD')
+        smtp_server = "smtp.gmail.com"
+        smtp_port = 587
+
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = user_email
+        msg['Subject'] = "[UvlHub] Email verify"
+        body = f"Hi, please verify your email with this link: {verification_link}"
+        msg.attach(MIMEText(body, 'plain'))
+
+        try:
+            server = smtplib.SMTP(smtp_server, smtp_port)
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, user_email, msg.as_string())
+            server.quit()
+            print("Verification email sent successfully")
+        except Exception as e:
+            print(f"Failed sending email: {e}")
