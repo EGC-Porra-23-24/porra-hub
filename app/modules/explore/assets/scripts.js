6f12c79d95ded4a4a03bcebae169c565db3d2d77
Ignacio González González <ignaciocefis2001@gmail.com>
2024-12-06 18:35:06 +0100

    feat(explore): Añadido comunidad si tiene en Explore

--- app/modules/explore/assets/scripts.js
+++ app/modules/explore/assets/scripts.js
@@ -58,6 +58,7 @@ function send_query() {
 
 
                     data.forEach(dataset => {
+                        console.log(dataset.url)
                         let card = document.createElement('div');
                         card.className = 'col-12';
                         card.innerHTML = `
@@ -96,8 +97,21 @@ function send_query() {
                                                 <p class="p-0 m-0">${author.name}${author.affiliation ? ` (${author.affiliation})` : ''}${author.orcid ? ` (${author.orcid})` : ''}</p>
                                             `).join('')}
                                         </div>
-
                                     </div>
+
+                                    ${dataset.community_id ? `
+                                    <div class="row mb-2">
+                                        <div class="col-md-4 col-12">
+                                            <span class="text-secondary">
+                                                Community
+                                            </span>
+                                        </div>
+                                        <div class="col-md-8 col-12">
+                                            <a href="${dataset.community_url}">
+                                                ${dataset.community_name}
+                                            </a>
+                                        </div>
+                                    </div>`: ''}
 
                                     <div class="row mb-2">
 
