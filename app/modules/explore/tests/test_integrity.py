e8026ab987a8922932a33f905bdd3c61a572d584
Pablo-Rufian-Jimenez <pabrufjim@alum.us.es>
2024-12-14 14:17:17 +0100

    test(dataset): Test unitarios de dsmetrics

--- app/modules/explore/tests/test_integrity.py
+++ app/modules/explore/tests/test_integrity.py
@@ -1,26 +1,26 @@
 import os
 from dotenv import load_dotenv
 import pytest
-from app import app, db
+from app import db
 from app.modules.auth.models import User
-from time import sleep
 from app.modules.dataset.models import Author, DSMetaData, DSMetrics, DataSet, PublicationType
 from datetime import datetime
 
 from app.modules.featuremodel.models import FMMetaData, FeatureModel
 from app.modules.hubfile.models import Hubfile
 
+
 @pytest.fixture(scope='module')
 def test_client(test_client):
     with test_client.application.app_context():
-        
-        #Create Users
+
+        # Create Users
         user1 = User(email='user1@example.com', password='1234')
         user2 = User(email='user2@example.com', password='1234')
         users = [user1, user2]
         db.session.add_all(users)
         db.session.commit()
-        
+
         # Create DSMetrics instances
         base_ds_metrics = DSMetrics(number_of_models=2, number_of_features=20)
         little_models_ds_metrics = DSMetrics(number_of_models=1, number_of_features=20)
@@ -31,7 +31,7 @@ def test_client(test_client):
                            little_features_ds_metrics, many_features_ds_metrics]
         db.session.add_all(ds_metrics_list)
         db.session.commit()
-        
+
         # Create DSMetaData instances
         ds_meta_data_list = [
             DSMetaData(
@@ -47,7 +47,7 @@ def test_client(test_client):
         ]
         db.session.add_all(ds_meta_data_list)
         db.session.commit()
-        
+
         # Create Author instances and associate with DSMetaData
         authors = [
             Author(
@@ -59,7 +59,7 @@ def test_client(test_client):
         ]
         db.session.add_all(authors)
         db.session.commit()
-        
+
         # Create DataSet instances and assign communities
         datasets = [
             DataSet(
@@ -72,7 +72,7 @@ def test_client(test_client):
         ]
         db.session.add_all(datasets)
         db.session.commit()
-        
+
         # Assume there are 12 UVL files, create corresponding FMMetaData and FeatureModel
         fm_meta_data_list = [
             FMMetaData(
@@ -87,7 +87,7 @@ def test_client(test_client):
         ]
         db.session.add_all(fm_meta_data_list)
         db.session.commit()
-        
+
         # Create Author instances and associate with FMMetaData
         fm_authors = [
             Author(
@@ -108,11 +108,12 @@ def test_client(test_client):
         ]
         db.session.add_all(feature_models)
         db.session.commit()
-        
+
+        # Files
         load_dotenv()
         working_dir = os.getenv('WORKING_DIR', '')
         src_folder = os.path.join(working_dir, 'app', 'modules', 'dataset', 'uvl_examples')
-        uvl_files=[]
+        uvl_files = []
         for i in range(14):
             file_name = f'file{i+1}.uvl'
             feature_model = feature_models[i]
@@ -128,63 +129,73 @@ def test_client(test_client):
             uvl_files.append(uvl_file)
         db.session.add_all(uvl_files)
         db.session.commit()
-        
+
     yield test_client
 
-#Check whether the provided list of datasets (as dicts) has all 7 datasets except the ones specified by the passed ids
+
+# Check whether the provided list of datasets (as dicts) has all 7 datasets except the ones specified by the passed ids
 def got_all_datasets_except(json, *args):
-    return set(map(lambda ds:ds['id'], json)) == {i for i in range(1,8) if i not in args}
+    return set(map(lambda ds: ds['id'], json)) == {i for i in range(1, 8) if i not in args}
+
 
 def test_no_filters(test_client):
     response = test_client.post("/explore", data='{}')
     assert response.status_code == 200
     json = response.json
-    assert len(json)==7
+    assert len(json) == 7
+
 
 def test_filter_by_start_date(test_client):
     response = test_client.post("/explore", data='{"min_creation_date":"2024-12-10"}')
     assert response.status_code == 200
     json = response.json
-    assert got_all_datasets_except(json , 2)
+    assert got_all_datasets_except(json, 2)
+
 
 def test_filter_by_end_date(test_client):
     response = test_client.post("/explore", data='{"max_creation_date":"2024-12-10"}')
     assert response.status_code == 200
     json = response.json
-    assert got_all_datasets_except(json , 3)
-    
+    assert got_all_datasets_except(json, 3)
+
+
 def test_filter_by_min_size(test_client):
     response = test_client.post("/explore", data='{"min_size":751}')
     assert response.status_code == 200
     json = response.json
-    assert got_all_datasets_except(json , 6)
-    
+    assert got_all_datasets_except(json, 6)
+
+
 def test_filter_by_max_size(test_client):
     response = test_client.post("/explore", data='{"max_size":1242}')
     assert response.status_code == 200
     json = response.json
-    assert got_all_datasets_except(json , 7)
+    assert got_all_datasets_except(json, 7)
 
-def test_filter_by_min_models(test_client):  
+
+def test_filter_by_min_models(test_client):
     response = test_client.post("/explore", data='{"min_models":2}')
     assert response.status_code == 200
     json = response.json
-    assert got_all_datasets_except(json , 4)
- 
-def test_filter_by_max_models(test_client):  
+    assert got_all_datasets_except(json, 4)
+
+
+def test_filter_by_max_models(test_client):
     response = test_client.post("/explore", data='{"max_models":2}')
     assert response.status_code == 200
     json = response.json
-    assert got_all_datasets_except(json , 5)
-    
-def test_filter_by_min_features(test_client):  
+    assert got_all_datasets_except(json, 5)
+
+
+def test_filter_by_min_features(test_client):
     response = test_client.post("/explore", data='{"min_features":20}')
     assert response.status_code == 200
     json = response.json
-    assert got_all_datasets_except(json , 6)
-    
-def test_filter_by_max_features(test_client):  
+    assert got_all_datasets_except(json, 6)
+
+
+def test_filter_by_max_features(test_client):
     response = test_client.post("/explore", data='{"max_features":20}')
     assert response.status_code == 200
     json = response.json
-    assert got_all_datasets_except(json , 5, 7)
+    assert got_all_datasets_except(json, 5, 7)
