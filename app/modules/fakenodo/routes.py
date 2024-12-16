a77d163df1c9c46134d460eeb42252bcb58d67b6
Albertoescobarsanchez <Albertoescobarsanchez@users.noreply.github.com>
2024-12-16 13:43:46 +0000

    fix(fakenodo): Auto-format modified Python files

--- app/modules/fakenodo/routes.py
+++ app/modules/fakenodo/routes.py
@@ -2,6 +2,6 @@ from flask import render_template
 from app.modules.fakenodo import fakenodo_bp
 
 
-@fakenodo_bp.route('/fakenodo', methods=['GET'])
+@fakenodo_bp.route("/fakenodo", methods=["GET"])
 def home():
-    return render_template('fakenodo/index.html')
+    return render_template("fakenodo/index.html")
