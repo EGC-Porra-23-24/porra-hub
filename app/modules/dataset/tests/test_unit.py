d4288e33dec35ec0da7ad6899373128bf36a97e0
jorgomde <jorgomde@users.noreply.github.com>
2024-12-16 12:45:06 +0000

    fix(dataset): Auto-format modified Python files

--- app/modules/dataset/tests/test_unit.py
+++ app/modules/dataset/tests/test_unit.py
@@ -665,6 +665,7 @@ def test_dsmetrics_both(dataset_service, current_user, test_app):
         dataset_service.create_from_form(form=form, current_user=current_user)
         mock_create_dsmetrics.assert_called_once_with(number_of_models=2, number_of_features=34)
 
+
 # Caso: URL válida de GitHub con archivo ZIP
 def test_upload_github_valid_zip(test_client, login):
     remember_token, session = login  # Obtenemos el token de autenticación
@@ -751,4 +752,3 @@ def test_upload_github_request_error(test_client, login):
     # Verificar la respuesta
     assert response.status_code == 500  # Aseguramos que el status code es 500
     assert response_data["error"].startswith("Error uploading file from GitHub")
-
